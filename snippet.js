5e28af69b53691a13183c33385c367230909fa30
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-20 21:55:43 +0530

    long input with multiple delimters

--- snippet.js
+++ snippet.js
@@ -1,4 +1,4 @@
-var str = "//[*][%]\n1*2%3" //The method can take up to two numbers, separated by commas, and will return their sum.
+var str = "//[*][%]\n1*2%3***********4,4,67\n" //The method can take up to two numbers, separated by commas, and will return their sum.
 //for example “” or “1” or “1,2” as inputs.(for an empty string it will return 0)
 function add(str) {
     let result = 0
@@ -13,13 +13,13 @@ function add(str) {
             return n;
         });
     //  replacing multiple delimeter, converting string to array , removing empty string, converting to integer
-    console.log("numberArray :", numberArray)
+    // console.log("numberArray :", numberArray)
 
     if (numberArray.some(v => v < 0)) {
         return "negatives not allowed"
     } else {
         result = result + numberArray.reduce((s, n) => s + n)//adding array elements
-        console.log("numberarray :", result)
+        // console.log("numberarray :", result)
         return result
     }
 
