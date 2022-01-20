7e990ddb23116d6eef9f1b4144d42e97b74739ec
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-20 21:23:23 +0530

    Support different delimiters

--- snippet.js
+++ snippet.js
@@ -1,8 +1,9 @@
-var str = "1\n2,3" //The method can take up to two numbers, separated by commas, and will return their sum.
+var str = "//;\n1;2\n5;7" //The method can take up to two numbers, separated by commas, and will return their sum.
 //for example “” or “1” or “1,2” as inputs.(for an empty string it will return 0)
 function add(str) {
     let result = 0
-    let numberArray = str.replace(/\n/gm, ',').split(',');//  replacing \n with and converting string to array,
+    let numberArray = str.replace(/\//g, '').replace(/[\n\r;]/gm, ',').split(',').filter(Boolean);//  replacing \n with and converting string to array,
+    console.log("numberArray :", numberArray)
     result = result + numberArray.reduce((s, n) => parseInt(s) + parseInt(n))//adding array elements
     console.log("numberarray :", result)
 }
