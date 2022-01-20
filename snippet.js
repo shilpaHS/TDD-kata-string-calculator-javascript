e2dacf58f812ee55ab491eee09fdc7bcfd288ab6
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-20 21:30:01 +0530

    negatives not allowed

--- snippet.js
+++ snippet.js
@@ -1,10 +1,17 @@
-var str = "//;\n1;2\n5;7" //The method can take up to two numbers, separated by commas, and will return their sum.
+var str = "//;\n1;2\n5;-7" //The method can take up to two numbers, separated by commas, and will return their sum.
 //for example “” or “1” or “1,2” as inputs.(for an empty string it will return 0)
 function add(str) {
     let result = 0
-    let numberArray = str.replace(/\//g, '').replace(/[\n\r;]/gm, ',').split(',').filter(Boolean);//  replacing \n with and converting string to array,
+    let numberArray = str.replace(/\//g, '').replace(/[\n\r;]/gm, ',').split(',').filter(Boolean).map(n => parseInt(n));
+    //  replacing multiple delimeter, converting string to array , removing empty string, converting to integer
     console.log("numberArray :", numberArray)
-    result = result + numberArray.reduce((s, n) => parseInt(s) + parseInt(n))//adding array elements
-    console.log("numberarray :", result)
+    if (numberArray.some(v => v < 0)) {
+        return "negatives not allowed"
+    } else {
+        result = result + numberArray.reduce((s, n) => s + n)//adding array elements
+        console.log("numberarray :", result)
+        return result
+    }
+
 }
-add(str)
+console.log(add(str))
