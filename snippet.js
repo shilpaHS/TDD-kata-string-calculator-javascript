e9632fa9ebe1384dbf1d221bd1871e99e3e83279
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-20 21:45:30 +0530

    Numbers bigger than 1000 should be ignored

--- snippet.js
+++ snippet.js
@@ -1,10 +1,18 @@
-var str = "//;\n1;2\n5;-7" //The method can take up to two numbers, separated by commas, and will return their sum.
+var str = "2,1003" //The method can take up to two numbers, separated by commas, and will return their sum.
 //for example “” or “1” or “1,2” as inputs.(for an empty string it will return 0)
 function add(str) {
     let result = 0
-    let numberArray = str.replace(/\//g, '').replace(/[\n\r;]/gm, ',').split(',').filter(Boolean).map(n => parseInt(n));
+    let numberArray = str.replace(/\//g, '').replace(/[\n\r;]/gm, ',').split(',').filter(Boolean)
+        .map(n => {
+            n = parseInt(n)
+            if (n > 1000) {
+                n = n - 1000
+            }
+            return n;
+        });
     //  replacing multiple delimeter, converting string to array , removing empty string, converting to integer
     console.log("numberArray :", numberArray)
+
     if (numberArray.some(v => v < 0)) {
         return "negatives not allowed"
     } else {
