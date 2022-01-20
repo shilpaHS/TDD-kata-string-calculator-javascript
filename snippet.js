d1ff32c2d3de8e9c8269eacad4e6d753673ea0fc
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-20 20:50:38 +0530

    first test case pass

---
+++ snippet.js
@@ -0,0 +1,9 @@
+var str = "" //The method can take up to two numbers, separated by commas, and will return their sum.
+//for example “” or “1” or “1,2” as inputs.(for an empty string it will return 0)
+function add(str) {
+    let result = 0
+    let numberArray = str.split(',');// converting string to array
+    result = result + numberArray.reduce((s, n) => parseInt(s) + parseInt(n))//adding array elements
+    console.log("numberarray :", result)
+}
+add(str)
